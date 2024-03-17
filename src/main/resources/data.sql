fa274407db1a790af2639a63c55da1b4b4af03e8
nahowo <nahowo@naver.com>
2024-03-17 11:57:23 +0900

    edit: data.sql 삭제

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into article(title, content, created_at, updated_at) values('우주','우주에 가자!',now(), now());
+insert into article(title, content, created_at, updated_at) values('아바타','바다에도 가보고 싶어. ',now(), now());
+insert into article(title, content, created_at, updated_at) values('bridgerton', '과거로도 가보고 싶어. ',now(), now());
